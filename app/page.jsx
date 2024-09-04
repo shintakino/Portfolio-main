75d413672318a654f10c5a236be002757c7b87e6
shintakino <joshuajaynarvaza@gmail.com>
2024-09-04 09:54:12 +0800

    count up functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Photo from "@/components/Photo";
 import Social from "@/components/Social";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import {FiDownload} from "react-icons/fi";
 
@@ -43,6 +44,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
